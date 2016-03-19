45dbf99e29b80b312d602fb680de7abb3d9d6abf
buptxge <uuytap@gmail.com>
2016-03-19 13:19:00 +0800

    change config

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 #coding:utf-8
-#USERNAME = "Your username"
-#PASSWORD = "Your password"
-#THREAD_URL = "Your thread url" #帖子的链接
-#COMMENT_CONTENT = "blalala.." #评论的内容
+USERNAME = "Your username"
+PASSWORD = "Your password"
+THREAD_URL = "Your thread url" #帖子的链接
+COMMENT_CONTENT = "blalala.." #评论的内容
 
-USERNAME = "zhangzhi918"
-PASSWORD = "zhangzhi12"
-THREAD_URL = "http://m.byr.cn/article/ParttimeJob/487139"
-CONTENT = "顶" #评论的内容
